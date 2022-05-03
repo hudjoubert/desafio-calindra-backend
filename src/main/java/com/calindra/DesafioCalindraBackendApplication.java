69068173353db0d7abfdab9df7463c84da0c0c85
hudjoubert <hudson_joubert@yahoo.com.br>
2022-05-03 02:41:17 -0300

    Initial commit

---
+++ src/main/java/com/calindra/DesafioCalindraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calindra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCalindraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCalindraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calindra/DesafioCalindraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calindra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCalindraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
